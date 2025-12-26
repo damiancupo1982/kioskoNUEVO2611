874835887b14baff13d6de37d0d8631649be251d
damiancupo1 <damiancupo@gmail.com>
2025-12-26 16:55:26 -0300

    Updated InventoryMovements.tsx

--- src/components/InventoryMovements.tsx
+++ src/components/InventoryMovements.tsx
@@ -294,8 +294,7 @@ export default function InventoryMovements() {
                         month: '2-digit',
                         year: 'numeric',
                         hour: '2-digit',
-                        minute: '2-digit',
-                        second: '2-digit'
+                        minute: '2-digit'
                       })}
                     </td>
                     <td className="px-6 py-4">
